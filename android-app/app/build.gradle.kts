4e01ee0143910884d63e32d94d47e908cf3c7c8f
sunggyeong <surreal0122@naver.com>
2026-06-05 10:28:36 +0900

    chore: point Android app to deployed backend

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,7 +13,8 @@ val localProperties = Properties().apply {
 }
 
 val apiBaseUrl: String = localProperties.getProperty("API_BASE_URL")
-    ?: "http://10.0.2.2:8000/"
+    ?: System.getenv("API_BASE_URL")
+    ?: "http://3.34.1.198:8000/"
 
 android {
     namespace = "com.capstone.mobilemeasure"
